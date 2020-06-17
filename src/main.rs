a4d90501a719843c3b1dce7dc6ddc5a227a55746
Sridaran Thoniyil <sri7thon@gmail.com>
2020-06-17 12:48:39 -0400

    Use 'directories' to find cache directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use directories::ProjectDirs;
+
 fn main() {
     println!("Hello, world!");
+    // step 1
+    // check if redshift mode or xrandr mode
+    let project_directory = ProjectDirs::from("", "Sridaran Thoniyil", "BrightnessControl");
+    // did not use if let because it would require the entire function to be indented
+
+    if let None = project_directory {
+        panic!("Cannot find base directory");
+    }
+
+    let project_directory = project_directory.unwrap();
+    // cache the mode
+    let cache_directory = project_directory.cache_dir();
+
+    println!("{}", cache_directory.to_str().unwrap());
 }
