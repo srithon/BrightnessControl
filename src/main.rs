ebd44a5dd7b5eb3b7974e00a15acfc4173b2d391
Sridaran Thoniyil <sri7thon@gmail.com>
2020-06-20 11:01:06 -0400

    Add code to increment/decrement brightness

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ use std::fs::{self, File, OpenOptions};
 use std::io::{Error, ErrorKind, Seek, Write, Read, Result};
 use std::fmt::Display;
 
+use std::cmp;
+
+enum DataValidatorResult<T> {
+    Valid(T),
+    Changed(T),
+}
+
+use DataValidatorResult::*;
+
 // where ....
-fn get_valid_data_or_write_default<T>(file: &mut File, data_validator: &dyn Fn(&String) -> Result<T>, default_value: T) -> Result<T>
+fn get_valid_data_or_write_default<T>(file: &mut File, data_validator: &dyn Fn(&String) -> Result<DataValidatorResult<T>>, default_value: T) -> Result<T>
     where T: Display {
     let file_contents = {
         // wrapping in a closure allows the inner else and the
         // outer else clauses to share the same code
         // here, we want to return None if the file does not exist
         // or if the file's contents are not readable as a number
-        (|| -> std::io::Result<T> {
+        (|| -> std::io::Result<DataValidatorResult<T>> {
             let mut buffer: Vec<u8> = Vec::new();
             file.read_to_end(&mut buffer)?;
 
@@ -26,14 +35,22 @@ fn get_valid_data_or_write_default<T>(file: &mut File, data_validator: &dyn Fn(&
         })()
     };
 
-    match file_contents {
-        Ok(contents) => Ok(contents),
-        Err(_) => {
-            // default to "regular" mode
-            file.seek(std::io::SeekFrom::Start(0))?;
-            write!(file, "{}", &default_value)?;
-            Ok(default_value)
-        },
+    if let Ok(Valid(contents)) = file_contents {
+        Ok(contents)
+    }
+    else {
+        file.seek(std::io::SeekFrom::Start(0))?;
+        let new_value = {
+            if let Ok(Changed(new_value)) = file_contents {
+                new_value
+            }
+            else {
+                default_value
+            }
+        };
+
+        write!(file, "{}", new_value)?;
+        Ok(new_value)
     }
 }
 
@@ -58,6 +75,18 @@ fn main() -> Result<()> {
 
     println!("{}", cache_directory.to_str().unwrap());
 
+    let mut args = std::env::args();
+
+    let increment = {
+        let arg = args.nth(1);
+        match arg {
+            Some(num) => num.parse::<i16>().unwrap_or(0),
+            None => 0,
+        }
+    };
+
+    println!("increment: {}", increment);
+
     let mode_filepath = cache_directory.join("mode");
     let brightness_filepath = cache_directory.join("brightness");
 
@@ -76,7 +105,7 @@ fn main() -> Result<()> {
             if let Ok(num) = data_in_file.parse::<u8>() {
                 if num == 0 || num == 1 {
                     println!("Successfully read in num: {}", num);
-                    return Ok(num);
+                    return Ok(Valid(num));
                 }
             }
 
@@ -94,14 +123,24 @@ fn main() -> Result<()> {
 
         get_valid_data_or_write_default(&mut brightness_file, &| data_in_file: &String | {
             // need to trim this because the newline character breaks the parse
-            if let Ok(num) = data_in_file.trim_end().parse::<u8>() {
+            if let Ok(num) = data_in_file.trim_end().parse::<i16>() {
                 println!("Brightness - Successfully read in num: {}", num);
-                if num <= 100 {
-                    return Ok(num);
+
+                if num >= 0 {
+                    // ensure range of [0, 100]
+                    // <<NOTE>> weird behavior incase of overflow
+                    let new_num = cmp::max(cmp::min(increment + num, 100), 0);
+
+                    if num == new_num {
+                        return Ok(Valid(num));
+                    }
+                    else {
+                        return Ok(Changed(new_num));
+                    }
                 }
             }
 
-            return Err(Error::new(ErrorKind::InvalidData, "Brightness out of bounds"));
+            return Err(Error::new(ErrorKind::InvalidData, "Invalid brightness"));
         }, 100)?
     };
 
