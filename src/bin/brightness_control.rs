e069964be5b449260171a60db8ea0b202213d22e
Sridaran Thoniyil <sri7thon@gmail.com>
2020-08-20 11:28:59 -0400

    Write input parsing code

--- src/bin/brightness_control.rs
+++ src/bin/brightness_control.rs
@@ -311,11 +311,80 @@ fn main() -> Result<()> {
         file_open_options
     };
 
-    let mut args = std::env::args();
+    let cli = get_cli_interface();
+    let args = std::env::args().collect::<Vec<String>>();
+    let matches = match cli.parse(&args[1..]) {
+        Ok(matches) => matches,
+        Err(error) => panic!(error.to_string())
+    };
 
-    let argument = args.nth(1).unwrap_or("".to_string());
+    let brightness = {
+        let increment = matches.opt_str("increment");
+        let decrement = matches.opt_str("decrement");
+        let set = matches.opt_str("set");
 
-    let increment = argument.parse::<i16>().unwrap_or(0);
+        let result: Result<Option<BrightnessChange>> = (|| {
+            let invalid_input = | message | {
+                Err(Error::new(ErrorKind::InvalidInput, message))
+            };
+
+            let duplicate_arguments = || {
+                invalid_input("Only one of {increment, decrement, set} can be used at the same time!")
+            };
+
+            if increment.is_some() || decrement.is_some() {
+                if set.is_some() || increment.is_some() && decrement.is_some() {
+                    return duplicate_arguments();
+                }
+
+                let (num, multiplier) = {
+                    if increment.is_some() {
+                        (increment.unwrap(), 1)
+                    }
+                    else {
+                        (decrement.unwrap(), -1)
+                    }
+                };
+
+                // "num" is a valid number
+                if let Ok(num) = num.parse::<i8>() {
+                    if num <= 100 && num >= -100 {
+                        Ok(Some(BrightnessChange::Adjustment(num * multiplier)))
+                    }
+                    else {
+                        invalid_input("Brightness adjustment must be between -100 and 100!")
+                    }
+                }
+                else {
+                    invalid_input("Invalid number passed as decrement/increment")
+                }
+            }
+            else if let Some(set) = set {
+                // ^ use if let here because there is only 1 variable to worry about
+                if decrement.is_some() {
+                    return duplicate_arguments();
+                }
+
+                if let Ok(new_brightness) = set.parse::<u8>() {
+                    Ok(Some(BrightnessChange::Set(new_brightness)))
+                }
+                else {
+                    invalid_input("Invalid brightness passed to set")
+                }
+            }
+            else {
+                Ok(None)
+            }
+        })();
+
+        result?
+    };
+
+    let program_input = ProgramInput {
+        brightness,
+        configure_display: matches.opt_present("configure-display"),
+        toggle_nightlight: matches.opt_present("toggle-nightlight")
+    };
 
     let program_state = ProgramState {
         cache_directory,
