32260f7388e7b78c63a3b38a6ecab515223019ea
Sridaran Thoniyil <sridaran@FedoraSridaran.ECB1750>
2020-10-16 18:29:12 -0400

    Create structs for handling logging

--- src/daemon.rs
+++ src/daemon.rs
@@ -42,6 +42,60 @@ lazy_static! {
     };
 }
 
+struct SocketMessageHolder {
+    messages: Vec<SocketMessage>,
+    socket: UnixStream
+}
+
+impl SocketMessageHolder {
+    fn new(socket: UnixStream) -> SocketMessageHolder {
+        SocketMessageHolder {
+            messages: Vec::with_capacity(5),
+            socket
+        }
+    }
+
+    fn queue_message<T>(&mut self, message: T, log_socket_error: bool)
+    where T: Into<String> {
+        self.messages.push(
+            SocketMessage {
+                message: message.into(),
+                log_socket_error
+            }
+        )
+    }
+
+    fn queue_success<T>(&mut self, message: T)
+    where T: Into<String> {
+        self.queue_message(message, true)
+    }
+
+    fn queue_error<T>(&mut self, message: T)
+    where T: Into<String> {
+        self.queue_message(message, false)
+    }
+
+    // NOTE remember to consume before it goes out of scope
+    fn consume(self) {
+        // write all messages to the socket
+        tokio::spawn(
+            async move {
+                for message_struct in self.messages.into_iter() {
+                    let message = message_struct.message;
+                    if let Err(e) = self.socket.write_all(&message.as_bytes()).await {
+                        if message_struct.log_socket_error {
+                            eprintln!("Failed to write \"{}\" to socket: {}", message, e);
+                        }
+                    }
+                }
+
+                // cleanup; close connection
+                let _ = self.socket.shutdown(std::net::Shutdown::Both);
+            }
+        );
+    }
+}
+
 struct SocketMessage {
     message: String,
     log_socket_error: bool
