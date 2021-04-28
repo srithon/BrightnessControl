96503455d74940edf22049004368ba022c941e44
Sridaran Thoniyil <sri7thon@gmail.com>
2021-04-28 18:38:39 -0400

    Update set-active description string get usage

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ fn get_cli_interface() -> clap::App<'static, 'static> {
             (about: "Holds commands that control BrightnessControl behavior for multiple monitors")
             (visible_alias: "m")
             (@group action =>
-                (@arg set_active: -s --("set-active") +takes_value value_name[monitor_adapter_name] "Sets the active monitor for use with \"brightness --active\"; use \"--get displays\" to see options")
+                (@arg set_active: -s --("set-active") +takes_value value_name[monitor_adapter_name] "Sets the active monitor for use with \"brightness --active\"; use \"get --displays\" to see options")
             )
         )
         (@subcommand nightlight =>
